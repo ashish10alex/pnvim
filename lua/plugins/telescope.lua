07d5f486e8f97f730aec34a7b1d35049f97f62e6
Ashish Alex <aalex@jaguarlandrover.com>
2024-01-08 01:31:07 +0000

    Add telescope config

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -6,16 +6,49 @@ return {
         "LinArcX/telescope-env.nvim",
 	},
 	config = function()
-        require('telescope').setup{
-             extensions = {
-                fzf = {
-                  fuzzy = true,                    -- false will only do exact matching
-                  override_generic_sorter = true,  -- override the generic sorter
-                  override_file_sorter = true,     -- override the file sorter
-                  case_mode = "smart_case",        -- or "ignore_case" or "respect_case"
-                                                   -- the default case_mode is "smart_case"
+        local grep_args = { '--hidden' }
+        require('telescope').setup {
+          defaults = {
+            prompt_prefix = "> ",
+            file_ignore_patterns = {".git/", ".cache", "%.o", "%.a", "%.out", "%.class",
+                "%rpdf", "%.mkv", "%.mp4", "%.zip", "node_modules", "yarn.lock"},
+          },
+          pickers = {
+            find_files = {
+              hidden = true,
+              git_ignore = false,
+            },
+            live_grep = {
+              additional_args = function(opts)
+                return grep_args
+              end
+            },
+            grep_string = {
+              additional_args = function(opts)
+                return grep_args
+              end
+            },
+            buffers = {
+              show_all_buffers = true,
+              sort_lastused = true,
+              theme = "dropdown",
+              previewer = false,
+              mappings = {
+                n = {
+                  ["<c-d>"] = "delete_buffer",
                 }
-             }
+              }
+            },
+          },
+          extensions = {
+            fzf = {
+              fuzzy = true,                    -- false will only do exact matching
+              override_generic_sorter = true,  -- override the generic sorter
+              override_file_sorter = true,     -- override the file sorter
+              case_mode = "smart_case",        -- or "ignore_case" or "respect_case"
+                                               -- the default case_mode is "smart_case"
+            }
+          },
         }
         require('telescope').load_extension('fzf')
         require('telescope').load_extension('env')
