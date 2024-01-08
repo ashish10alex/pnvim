7fd50a84aaf39844fa84ea325e33989c8bce19f2
Ashish Alex <aalex@jaguarlandrover.com>
2024-01-08 01:41:53 +0000

    script to compile dataform

---
+++ lua/config/jlr.lua
@@ -0,0 +1,17 @@
+
+local read_file = function(path)
+  local file = io.open(path, "rb")
+  if not file then return nil end
+  local content = file:read "*a" -- *a or *all reads the whole file
+  file:close()
+  return content
+end
+
+
+local compile_dataform = function()
+    local dataform_compile_cmd_path = os.getenv("HOME") .. "/.config/nvim/lua/dataform_compile_cmd.txt"
+    local dataform_compile_cmd = read_file(dataform_compile_cmd_path)
+    local output = vim.fn.system(dataform_compile_cmd) -- output is sent to a file using >>
+    print("Compiled dataform output to /tmp/temp.sqlx")
+end
+vim.api.nvim_create_user_command("CompileDataform", compile_dataform, {})
