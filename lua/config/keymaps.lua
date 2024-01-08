6d0d65954f7e2c7f3cda92fe20d5e294fa2740c7
Ashish Alex <aalex@jaguarlandrover.com>
2024-01-08 08:21:12 +0000

    Update comment

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -10,7 +10,7 @@ vim.keymap.set("n", "<C-u>", "<C-u>zz")
 
 -- copies to system clipboard in normal, visual modes
 vim.keymap.set({"n", "v"}, "Y", [["+y]])
--- copies to system clipboard in normal, visual modes from current cursor to end of line
+-- copies current line to system clipboard in normal & visual modes 
 vim.keymap.set("n", "<leader>Y", [["+Y]])
 
 
