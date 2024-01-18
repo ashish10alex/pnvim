3705c2fa7f5b855106a45cd77144c1b0b7fce837
Ashish Alex <aalex@jaguarlandrover.com>
2024-01-18 17:03:36 +0000

    Word under cursor search telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -67,11 +67,28 @@ return {
 
 		vim.keymap.set('n', '<leader>f', builtin.find_files, {})
         vim.keymap.set('n', '<leader>b', builtin.buffers, {})
+
         vim.keymap.set('n', '<leader>grep', builtin.live_grep, {})
+        vim.keymap.set('n', '<leader>greb', builtin.current_buffer_fuzzy_find, {})
+        vim.keymap.set('n', '<leader>ps', function()
+        builtin.grep_string({search = vim.fn.input("Grep > ") })
+        end)
+
+        vim.keymap.set('n', '<leader>grw', function()
+            local word = vim.fn.expand("<cword>")
+            builtin.grep_string({search = word})
+        end)
+
+        vim.keymap.set('n', '<leader>grW', function()
+            local word = vim.fn.expand("<cWORD>")
+            builtin.grep_string({search = word})
+        end)
+
         -- vim.keymap.set('n', '<leader>greb', builtin.live_grep, {grep_on_open_files = true})
         vim.keymap.set('n', '<leader>h', builtin.help_tags, {})
         vim.keymap.set('n', '<leader>gf', builtin.git_files, {})
         vim.keymap.set('n', '<leader>gd', builtin.lsp_definitions, {})
+        vim.keymap.set('n', '<leader>de', builtin.diagnostics, {})
 
 	end,
 }
