a5c1047678b0ef475fa7e14059cc317556de877a
Ashish Alex <aalex@jaguarlandrover.com>
2024-01-19 23:09:19 +0000

    Update to dataform compile command

---
+++ lua/config/dataform_compile_all.sh
@@ -0,0 +1,3 @@
+echo "-- $(date)" > /tmp/temp.sqlx
+dataform compile --json | jq -r '.tables | map("-- \(.fileName)\n-- \(.tags)\n\(.query) ;") | join("\n")'  >> /tmp/temp.sqlx ;
+dataform compile --json | jq -r '.assertions | map("-- \(.fileName)\n-- \(.tags)\n\(.query) ;") | join("\n")'  >>  /tmp/temp.sqlx;
