837487a45c2d7b633b96eddd23bcdca36bb8eca4
Ashish Alex <aalex@jaguarlandrover.com>
2024-01-20 12:16:38 +0000

    Open Dataform compile output in vsplit

--- lua/config/jlr.lua
+++ lua/config/jlr.lua
@@ -12,5 +12,17 @@ local compile_dataform = function()
     local dataform_compile_cmd = read_file(dataform_compile_cmd_path)
     local output = vim.fn.system(dataform_compile_cmd) -- output is sent to a file using >>
     print("Compiled dataform output to /tmp/temp.sqlx")
+
+    local buf_name = "/tmp/temp.sqlx"
+    local bufnr = vim.fn.bufnr(buf_name, true)
+    if bufnr ~= -1 then
+        print("Buffer number: " .. bufnr)
+        vim.api.nvim_command("vsplit " .. buf_name)
+        vim.diagnostic.set(1, 0, {{bufnr=bufnr, lnum=55, col=1, end_col=2, severity = vim.diagnostic.severity.ERROR, message = "Syntax error: Missing closing parenthesis",}}, {}) -- TODO: get the line number from another cli output
+    else
+        print("Buffer number: " .. bufnr)
+        vim.api.nvim_command("edit " .. buf_name)
+    end
 end
 vim.api.nvim_create_user_command("CompileDataform", compile_dataform, {})
+
