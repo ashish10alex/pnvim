3eb48c5a521dc3ebdb4ebb3326f70bd171b8acad
Ashish Alex <aalex@jaguarlandrover.com>
2024-01-20 17:43:22 +0000

    Get vsplit CompileDataform working

--- lua/config/jlr.lua
+++ lua/config/jlr.lua
@@ -7,21 +7,37 @@ local read_file = function(path)
   return content
 end
 
+local find_buffer_by_name = function(name)
+  for _, bufnr in ipairs(vim.api.nvim_list_bufs()) do
+    local buf_name = vim.api.nvim_buf_get_name(bufnr)
+    -- print("searching buffer" .. buf_name .. " with bufnr: " .. bufnr)
+    if buf_name == name then
+      -- print("found buffer" .. buf_name .. " with bufnr: " .. bufnr)
+      return bufnr
+    end
+  end
+  return -1
+end
+
 local compile_dataform = function()
     local dataform_compile_cmd_path = os.getenv("HOME") .. "/.config/nvim/lua/config/dataform_compile_wt_tag.sh"
     local dataform_compile_cmd = read_file(dataform_compile_cmd_path)
     local output = vim.fn.system(dataform_compile_cmd) -- output is sent to a file using >>
-    print("Compiled dataform output to /tmp/temp.sqlx")
 
-    local buf_name = "/tmp/temp.sqlx"
-    local bufnr = vim.fn.bufnr(buf_name, true)
-    if bufnr ~= -1 then
-        print("Buffer number: " .. bufnr)
+    local buf_name = "/private/tmp/temp.sqlx"
+    local bufnr = find_buffer_by_name(buf_name)
+
+    if bufnr ~= -1 then -- buffer already open
+        -- TODO: clear previous the diagnostics ?
+        vim.diagnostic.set(1, 0, {{bufnr=bufnr, lnum=55, col=1, end_col=2, severity = vim.diagnostic.severity.ERROR, message = "Syntax error NEW: BigQuery test",}}, {}) -- TODO: get the line number from another cli output
+        vim.api.nvim_command("wincmd h") -- move the cursor to this buffer and execute edit to load the file
+        vim.api.nvim_command("edit")
+
+    else -- buffer not open, create a new one
         vim.api.nvim_command("vsplit " .. buf_name)
-        vim.diagnostic.set(1, 0, {{bufnr=bufnr, lnum=55, col=1, end_col=2, severity = vim.diagnostic.severity.ERROR, message = "Syntax error: Missing closing parenthesis",}}, {}) -- TODO: get the line number from another cli output
-    else
-        print("Buffer number: " .. bufnr)
-        vim.api.nvim_command("edit " .. buf_name)
+        vim.diagnostic.set(1, 0, {{bufnr=bufnr, lnum=55, col=1, end_col=2, severity = vim.diagnostic.severity.ERROR, message = "Syntax error: BigQuery test",}}, {}) -- TODO: get the line number from another cli output
+        vim.api.nvim_command("wincmd h") -- move the cursor to this buffer and execute edit to load the file
+
     end
 end
 vim.api.nvim_create_user_command("CompileDataform", compile_dataform, {})
