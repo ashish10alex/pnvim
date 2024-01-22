86539588fb7891bd2c2811970dc75f2e8b5be531
Ashish Alex <aalex@jaguarlandrover.com>
2024-01-22 15:39:07 +0000

    Show tags

--- lua/config/dataform_compile_wt_tag.sh
+++ lua/config/dataform_compile_wt_tag.sh
@@ -1,4 +1,4 @@
 echo "-- $(date)" > /tmp/temp.sqlx
-dataform compile --json | jq --arg tag "HOLDS" -r '.tables[] | select(.tags | any(. == $tag)) | "--" + .fileName + "\n" + .query + "; \n" '  >> /private/tmp/temp.sqlx ;
-dataform compile --json | jq --arg tag "HOLDS" -r '.assertions[] | select(.tags | any(. == $tag)) | "--" + .fileName + "\n" + .query + "; \n" ' >> /private/tmp/temp.sqlx
+dataform compile --json | jq --arg tag "DPR" -r '.tables[] | select(.tags | any(. == $tag)) | "-- " + .fileName + "\n" + "-- tags " + (.tags | tojson) + "\n" + .query + "; \n" '  >> /private/tmp/temp.sqlx ;
+dataform compile --json | jq --arg tag "DPR" -r '.assertions[] | select(.tags | any(. == $tag)) | "-- " + .fileName + "\n" + "-- tags " + (.tags | tojson) + "\n" + .query + "; \n" ' >> /private/tmp/temp.sqlx
 
