64d90673c12d2dab38cfae98fa655a96162a9c4d
Ashish Alex <aalex@jaguarlandrover.com>
2024-03-11 23:00:17 +0000

    new plugins

---
+++ lua/plugins/lsp_lines.lua
@@ -0,0 +1,19 @@
+return {
+    "https://git.sr.ht/~whynothugo/lsp_lines.nvim",
+    config = function()
+        require("lsp_lines").setup()
+        vim.diagnostic.config(
+            {
+                virtual_lines = false,
+            }
+        )
+        vim.keymap.set(
+            "",
+            "<Leader>l",
+            function()
+                require("lsp_lines").toggle()
+            end,
+            { desc = "Toggle lsp_lines" }
+        )
+    end,
+}
