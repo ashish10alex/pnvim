72536ddbd601bfc675899e76e7cb6a54965ea2e1
Ashish Alex <aalex@jaguarlandrover.com>
2024-04-27 17:13:51 +0100

    Use env variable

--- lua/plugins/dataform.lua
+++ lua/plugins/dataform.lua
@@ -4,7 +4,7 @@ return {
     config = function()
         local opts = {
             timeout            = 10000,
-            gcp_project_id_dev = "jlr-it-scanalytics-dev",
+            gcp_project_id_dev = vim.env.GCP_PROJECT_ID_DEV,
             sql_out_buf_path   = "/tmp/output.sql",
             dj_cli_path        = "$HOME/bin/dj",
             compiled_json_path = "/tmp/compiled.json",
