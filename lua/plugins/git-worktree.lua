6c4a6236f4b1c70b231828bf2f5c8e69022efdb2
Ashish Alex <aalex@jaguarlandrover.com>
2024-04-28 21:12:58 +0100

    Change keymaps for git-worktree

--- lua/plugins/git-worktree.lua
+++ lua/plugins/git-worktree.lua
@@ -5,7 +5,7 @@ return {
     },
     config = function()
         pcall(require("telescope").load_extension("git_worktree"))
-        vim.api.nvim_set_keymap('n', '<leader>gw', ':lua require("telescope").extensions.git_worktree.git_worktrees()<CR>', { noremap = true, silent = true })
-        vim.api.nvim_set_keymap('n', '<leader>gcw', ':lua require("telescope").extensions.git_worktree.create_git_worktree()<CR>', { noremap = true, silent = true })
+        vim.api.nvim_set_keymap('n', '<leader>gwl', ':lua require("telescope").extensions.git_worktree.git_worktrees()<CR>', { noremap = true, silent = true })
+        vim.api.nvim_set_keymap('n', '<leader>gwc', ':lua require("telescope").extensions.git_worktree.create_git_worktree()<CR>', { noremap = true, silent = true })
     end,
 }
