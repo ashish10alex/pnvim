ebfca0b5180c4bc0c502fab2e435351fd840bd62
Ashish Alex <aalex@jaguarlandrover.com>
2024-05-04 16:36:15 +0100

    Switch to git-worktree fork to be compatible with new telescope

--- lua/plugins/git-worktree.lua
+++ lua/plugins/git-worktree.lua
@@ -1,11 +1,15 @@
 return {
-     'ThePrimeagen/git-worktree.nvim',
-     dependencies = {
+    'awerebea/git-worktree.nvim',
+    branch = "handle_changes_in_telescope_api",
+    dependencies = {
         'nvim-telescope/telescope.nvim', branch = 'master',
     },
     config = function()
         pcall(require("telescope").load_extension("git_worktree"))
-        vim.api.nvim_set_keymap('n', '<leader>gwl', ':lua require("telescope").extensions.git_worktree.git_worktrees()<CR>', { noremap = true, silent = true })
-        vim.api.nvim_set_keymap('n', '<leader>gwc', ':lua require("telescope").extensions.git_worktree.create_git_worktree()<CR>', { noremap = true, silent = true })
+        vim.api.nvim_set_keymap('n', '<leader>gwl',
+            ':lua require("telescope").extensions.git_worktree.git_worktrees()<CR>', { noremap = true, silent = true })
+        vim.api.nvim_set_keymap('n', '<leader>gwc',
+            ':lua require("telescope").extensions.git_worktree.create_git_worktree()<CR>',
+            { noremap = true, silent = true })
     end,
 }
